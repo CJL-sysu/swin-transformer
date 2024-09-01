be27625b1947b50fe66e2fe9e363393358b668e7
CJL <2743880348@qq.com>
2024-09-01 17:03:11 +0800

    tidying code

--- train.py
+++ train.py
@@ -28,15 +28,31 @@ transform = transforms.Compose([
     transforms.Normalize(0.5, 0.5)
 ])
 
-trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
-                                          shuffle=True)
+trainset = torchvision.datasets.CIFAR100(
+    root='./data',
+    train=True,
+    download=True,
+    transform=transform
+)
 
-testset = torchvision.datasets.CIFAR100(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64,
-                                         shuffle=True)
+trainloader = torch.utils.data.DataLoader(
+    trainset,
+    batch_size=64,
+    shuffle=True
+)
+
+testset = torchvision.datasets.CIFAR100(
+    root='./data',
+    train=False,
+    download=True,
+    transform=transform
+)
+
+testloader = torch.utils.data.DataLoader(
+    testset,
+    batch_size=64,
+    shuffle=True
+)
 
 # 3. 定义ViT模型
 best_prec, epoch = 0, 0
