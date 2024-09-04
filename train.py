ac1061f65cbe1be3389c93962e7a8616efebccbd
CJL <2743880348@qq.com>
2024-09-04 16:01:18 +0800

    add weight decay

--- train.py
+++ train.py
@@ -11,6 +11,8 @@ arch = "swin_v2_t"
 dataset = 'imagenet100'
 logger = tlib.Logger(output='stdout')
 pretrained = False
+drop_out_p = 0.35
+weight_decay_rt = 0.0001
 # Select Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Prepare Dataset
@@ -45,6 +47,10 @@ def prepare_model(model_name, num_class):
         setattr(base_model, "fc", nn.Linear(feature_dim, num_class))
     elif 'swin' in model_name:
         base_model.head = nn.Linear(base_model.head.in_features, num_class)
+        for name, module in base_model.named_modules():
+            if isinstance(module, nn.Dropout):
+                # print(f"Before: {module}")
+                module.p = drop_out_p  # 修改 dropout 概率
     else:
         raise ValueError(f"Model {model_name} not supported")
     return base_model
@@ -54,7 +60,7 @@ logger.trace(model)
 # Train Model
 loss = torch.nn.CrossEntropyLoss(reduction='none').to(device)
 num_epochs = 1000
-updater = torch.optim.SGD(model.parameters(), lr=0.01)
+updater = torch.optim.SGD(model.parameters(), lr=0.004, weight_decay=weight_decay_rt)
 checkpoint_manager = tlib.CheckpointManager(
     arch=arch,
     dataset=dataset,
